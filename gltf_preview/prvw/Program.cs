42745d849b50bfe184eda3d846d21adb2dce2f16
Alexanderexe <cronergy100@mail.ru>
2021-09-27 00:05:29 +0300

    initial GL rendered

---
+++ gltf_preview/prvw/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace prvw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Textures",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+
+
+        }
+    }
+}
